7da2415e2c806df2f00a6fe0a7e553ec6d0e1969
Kuno Claes (MBP) <kuno@icodes.dev>
2025-01-15 13:32:44 +0100

    Initial Commit

---
+++ functions/reportDevice.py
@@ -0,0 +1,25 @@
+import requests
+from dotenv import load_dotenv
+import os
+import uuid
+
+load_dotenv()
+API_URL = os.getenv("API_URL")
+
+
+def reportDevice():
+    mac_address = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8 * 6, 8)][::-1])
+    data = {
+        "beehiveName": "Baguette",
+        "latitude": 0,
+        "longitude": 0,
+        "iotId": mac_address
+    }
+    response = requests.post(f"{API_URL}/api/beehives", json=data)
+
+    try:
+        response_json = response.json()
+    except requests.exceptions.JSONDecodeError:
+        response_json = None
+
+    print(response.status_code, response_json)
