fd64893d3d11f0c0126308790462ae9a92f4c03b
Kuno Claes (MBP) <kuno@icodes.dev>
2025-01-16 13:05:33 +0100

    Added turn off relay after 1 second

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import time
 from functions import *
 
 # Report the device information to the server
 reportDevice()
+
+# Turn on the relay
 switchRelay(True)
+
+# Wait for 1 second
+time.sleep(1)
+
+# Turn off the relay
+switchRelay(False)
