d6d82bcc51593684343fb56da86abee27e4322c0
Kuno Claes (MBP) <kuno@icodes.dev>
2025-01-17 09:17:25 +0100

    changed coordinates for demo

--- functions/reportDevice.py
+++ functions/reportDevice.py
@@ -10,9 +10,9 @@ API_URL = os.getenv("API_URL")
 def reportDevice():
     mac_address = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8 * 6, 8)][::-1])
     data = {
-        "beehiveName": "Baguette",
-        "latitude": 0,
-        "longitude": 0,
+        "beehiveName": "Beehive Demo",
+        "latitude": 51.160154648639526,
+        "longitude": 5.003539056066445,
         "iotId": mac_address
     }
     response = requests.post(f"{API_URL}/api/beehives", json=data)
